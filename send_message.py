49fa99b0e27fa8aa047aabf3105290e2f09beee1
iranianclever <iranianclever@gmail.com>
2020-03-24 17:16:56 +0430

    Version 1.0 with gui qt5

--- send_message.py
+++ send_message.py
@@ -9,10 +9,38 @@ class SendMessage:
         ''' initialization '''
         # path of file numbers
         self.__PATH = 'numbers.json'
+        # Path of API Key
+        self.__PATH_API = 'api'
         # api key
-        self.__API_key = '2F79766A61495263422B4655626F773361447A566D68534C4E43615A44414C4A7737583856693334366E493D'
+        self.API_key = self.get_api_key()
         # url from web service
-        self.__URL = f'https://api.kavenegar.com/v1/{self.__API_key}/sms/send.json'
+        self.__URL = f'https://api.kavenegar.com/v1/{self.API_key}/sms/send.json'
+
+    def get_api_key(self):
+        """ get api key from file """
+        api = ''
+        # Check and open api file
+        try:
+            # Open file API
+            with open(self.__PATH_API) as f:
+                api = f.read()
+        except FileNotFoundError:
+            open(self.__PATH_API, 'w')
+
+        # Check API key for return
+        if api:
+            return api
+        return None
+
+    def save_api_key(self, api):
+        """ Get api key and save to file api """
+        # Set new api key to file
+        with open(self.__PATH_API, 'w') as f:
+            f.write(api)
+        # Get again api key and assign to main api
+        self.API_key = self.get_api_key()
+        # url from web service
+        self.__URL = f'https://api.kavenegar.com/v1/{self.API_key}/sms/send.json'
 
     def return_persons(self):
         ''' returns numbers of json file with dictionary format '''
@@ -21,28 +49,29 @@ class SendMessage:
         # check for exist file or no
         try:
             # reading file
-            with open(self.__PATH) as f:
+            with open(self.__PATH, encoding='utf-8') as f:
                 # convert to string
                 text = f.read()
                 # if not empty, loads text to json format
                 if text:
-                    persons = json.loads(text)
+                    persons = json.loads(text, encoding='utf-8')
         # if not exist, create file
         except FileNotFoundError:
-            open(self.__PATH, 'w')
+            open(self.__PATH, 'w', encoding='utf-8')
         return persons
 
     def add_number(self, name, number):
         ''' add number and name to list '''
         # read all list of numbers
         persons = self.return_persons()
-        with open(self.__PATH, 'w') as f:
+        with open(self.__PATH, 'w', encoding='utf-8') as f:
             # create new numbers
             persons[name] = number
             # convert to json format
-            json_format = json.dumps(persons)
+            json_format = json.dumps(
+                persons, ensure_ascii=False).encode('utf-8')
             # writing on file
-            f.write(json_format)
+            f.write(json_format.decode())
 
     def remove_number(self, name):
         ''' remove a number from list '''
@@ -51,7 +80,7 @@ class SendMessage:
         # status for removed or no
         is_removed = False
         # open file for check
-        with open(self.__PATH, 'w') as f:
+        with open(self.__PATH, 'w', encoding='utf-8') as f:
             # loop from container of persons with keys(name)
             for person_name in persons.keys():
                 # check the name to match
@@ -61,9 +90,10 @@ class SendMessage:
                     # status is true
                     is_removed = True
                     # convert list to json format
-                    json_format = json.dumps(persons)
+                    json_format = json.dumps(
+                        persons, ensure_ascii=False).encode('utf-8')
                     # write file
-                    f.write(json_format)
+                    f.write(json_format.decode())
                     # break looping
                     break
         return is_removed
@@ -75,7 +105,7 @@ class SendMessage:
         # status for modified or no
         is_modify = False
         # open file for check
-        with open(self.__PATH, 'w') as f:
+        with open(self.__PATH, 'w', encoding='utf-8') as f:
             # loop from container of persons with keys(name)
             for person_name in persons.keys():
                 # check the name to match
@@ -85,9 +115,10 @@ class SendMessage:
                     # status is true
                     is_modify = True
                     # convert list to json format
-                    json_format = json.dumps(persons)
+                    json_format = json.dumps(
+                        persons, ensure_ascii=False).encode('utf-8')
                     # write file
-                    f.write(json_format)
+                    f.write(json_format.decode())
                     # break looping
                     break
         return is_modify
@@ -96,6 +127,8 @@ class SendMessage:
         ''' sending message '''
         # persons with dictionary format
         persons = self.return_persons()
+        # Holder status of sending message
+        status = {}
         # sending message to the person
         for name, number in persons.items():
             # valid data for send to web service
@@ -107,21 +140,7 @@ class SendMessage:
             response = requests.post(self.__URL, data=payload)
             # check message correctly
             if response.status_code == 200:
-                print('ارسال شد', name, response.status_code)
-                # return True
+                status[name] = f'ارسال شد | کد وضعیت : {response.status_code}'
             else:
-                print('عدم ارسال', name, response.status_code)
-                # return False
-
-
-send_message = SendMessage()
-
-send_message.add_number('Amirhossein', '09100000000')
-send_message.add_number('Sara', '09100000001')
-
-send_message.remove_number('Amirhossein')
-
-status = send_message.modify_number('Sara', '09130000000')
-
-
-# send_message.send("سلام امروز من با شما کار دارم تماس بگیرید")
+                status[name] = f'ارسال نشده | کد وضعیت : {response.status_code}'
+        return status
